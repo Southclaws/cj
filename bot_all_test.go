6d65337f1a41914fa00e466f896d7633dc996a1e
Southclaw <southclawjk@gmail.com>
2017-10-10 20:57:49 +0100

    updated travis config

--- bot_all_test.go
+++ bot_all_test.go
@@ -10,28 +10,19 @@ var app App
 
 func TestMain(m *testing.M) {
 	app = App{
-		config:     Config{},
+		config: Config{
+			MongoHost:       "localhost",
+			MongoPort:       "27017",
+			MongoName:       "cj",
+			MongoUser:       "root",
+			MongoPass:       "",
+			MongoCollection: "cj",
+		},
 		httpClient: &http.Client{},
-	}
-
-	configLocation := os.Getenv("CONFIG_FILE")
-	if configLocation == "" {
-		configLocation = "config_example.json"
-	}
-
-	dbLocation := os.Getenv("DB_FILE")
-	if dbLocation == "" {
-		dbLocation = "users_test.db"
 	}
 
 	app.ConnectDB()
 	app.LoadLanguages()
 
-	ret := m.Run()
-
-	if err := os.Remove(dbLocation); err != nil {
-		panic(err)
-	}
-
-	os.Exit(ret)
+	os.Exit(m.Run())
 }
