8cb324c4b2448df64224c4aafccf4c52e542c194
iAmir <amirabgta@gmail.com>
2018-06-17 22:41:16 +0430

    Fixed bot_cmd_wiki.go issues

--- bot_cmd_wiki.go
+++ bot_cmd_wiki.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+
     "io/ioutil"
     "net/http"
     "strings"
@@ -11,20 +11,20 @@ import (
 
 func commandWiki(cm CommandManager, args string, message discordgo.Message, contextual bool) (bool, bool, error) {
 	
-	if args == nil {
+	if len(args) == 0 {
 		cm.App.discordClient.ChannelMessageSend(message.ChannelID, "USAGE : /wiki [function/callback/article_name]")
 		return false, false, nil
 	}
 
-	wikiUrl := strings.Replace("http://wiki.sa-mp.com/wiki/" + args, " ", "_", -1)
+	wikiURL := strings.Replace("http://wiki.sa-mp.com/wiki/" + args, " ", "_", -1)
 
-	response, _ := http.Get(wikiUrl)
+	response, _ := http.Get(wikiURL)
 	bodyText, _ := ioutil.ReadAll(response.Body)
 
 	if strings.Contains(string(bodyText), "There is currently no text in this page, you can") {
 		cm.App.discordClient.ChannelMessageSend(message.ChannelID, "SA:MP Wiki | " + args + "\n- This article does not exist")
 	} else {
-		cm.App.discordClient.ChannelMessageSend(message.ChannelID, "SA:MP Wiki | " + args + "\n" + wikiUrl)
+		cm.App.discordClient.ChannelMessageSend(message.ChannelID, "SA:MP Wiki | " + args + "\n" + wikiURL)
 	} 
 	
 	return true, true, nil
