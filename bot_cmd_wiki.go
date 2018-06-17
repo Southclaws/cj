1272be92ca05edc9b15864c22243388158005765
Southclaws <southclawjk@gmail.com>
2018-06-17 19:32:47 +0100

    gofmt

--- bot_cmd_wiki.go
+++ bot_cmd_wiki.go
@@ -1,31 +1,30 @@
 package main
 
 import (
+	"io/ioutil"
+	"net/http"
+	"strings"
 
-    "io/ioutil"
-    "net/http"
-    "strings"
-
-    "github.com/bwmarrin/discordgo"
+	"github.com/bwmarrin/discordgo"
 )
 
 func commandWiki(cm CommandManager, args string, message discordgo.Message, contextual bool) (bool, bool, error) {
-	
+
 	if len(args) == 0 {
 		cm.App.discordClient.ChannelMessageSend(message.ChannelID, "USAGE : /wiki [function/callback/article_name]")
 		return false, false, nil
 	}
 
-	wikiURL := strings.Replace("http://wiki.sa-mp.com/wiki/" + args, " ", "_", -1)
+	wikiURL := strings.Replace("http://wiki.sa-mp.com/wiki/"+args, " ", "_", -1)
 
 	response, _ := http.Get(wikiURL)
 	bodyText, _ := ioutil.ReadAll(response.Body)
 
 	if strings.Contains(string(bodyText), "There is currently no text in this page, you can") {
-		cm.App.discordClient.ChannelMessageSend(message.ChannelID, "SA:MP Wiki | " + args + "\n- This article does not exist")
+		cm.App.discordClient.ChannelMessageSend(message.ChannelID, "SA:MP Wiki | "+args+"\n- This article does not exist")
 	} else {
-		cm.App.discordClient.ChannelMessageSend(message.ChannelID, "SA:MP Wiki | " + args + "\n" + wikiURL)
-	} 
-	
+		cm.App.discordClient.ChannelMessageSend(message.ChannelID, "SA:MP Wiki | "+args+"\n"+wikiURL)
+	}
+
 	return true, true, nil
 }
