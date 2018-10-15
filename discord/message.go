b4027d66897f91c99ca728dd18b768f8431ed0f9
BigETI <bigeti.ethem@gmail.com>
2018-10-15 19:28:13 +0200

    Muted tags are now bold.

--- discord/message.go
+++ discord/message.go
@@ -7,8 +7,8 @@ import (
 // ChannelMessageSend filters out everyone and here mentions
 func (s *Session) ChannelMessageSend(channelID string, content string) {
 	r := strings.NewReplacer(
-		"@everyone", "@(everyone)",
-		"@here", "@(here)",
+		"@everyone", "@**everyone**",
+		"@here", "@**here**",
 	)
 	//nolint:errcheck
 	s.S.ChannelMessageSend(channelID, r.Replace(content))
