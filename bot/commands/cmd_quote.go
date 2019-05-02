b303455605536795cb8d84e01d132cb4c05a43b0
J0shES <J0shES@protonmail.com>
2019-05-02 17:42:08 +0100

    Added certain channels to be restricted from /quote

--- bot/commands/cmd_quote.go
+++ bot/commands/cmd_quote.go
@@ -20,9 +20,21 @@ func (cm *CommandManager) commandQuote(
 		ok          bool
 		randmessage storage.ChatLog
 		messagerr   error
+		restricted  bool
 	)
 
+	restrictedChannels := [11]string{
+		"457943077789892649",
+		"536617253286707210",
+		"376371371795546112",
+		"361640153489473540",
+		"531855907374759936",
+		"531860517816500235",
+		"572476607772491786",
+	}
+
 	for ok == false {
+		restricted = false
 		randmessage, messagerr = cm.Storage.GetRandomMessage()
 		if messagerr != nil {
 			cm.Discord.ChannelMessageSend(message.ChannelID, "Could not get a quote.")
@@ -31,7 +43,15 @@ func (cm *CommandManager) commandQuote(
 
 		// Misses stuff like verify and doesn't allow commands to be sent either.
 		if len(randmessage.Message) > 6 && strings.Index(randmessage.Message, "/") != 0 {
-			ok = true
+			for _, channel := range restrictedChannels {
+				if randmessage.DiscordChannel == channel {
+					restricted = true
+				}
+			}
+
+			if !restricted {
+				ok = true
+			}
 		}
 	}
 
