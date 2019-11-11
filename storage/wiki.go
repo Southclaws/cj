7dc12ffd7461e4b5b587ed4d8f91fefa8b0e75bf
dakyskye <lashakanteladze339@protonmail.com>
2019-11-11 23:09:30 +0400

    rewrote wiki pulling workflow

--- storage/wiki.go
+++ storage/wiki.go
@@ -1,62 +1,44 @@
 package storage
 
 import (
-	"context"
-	"time"
-
 	"gopkg.in/src-d/go-git.v4"
 )
 
-// EnsureWiki ensures that wiki is cloned and pulls it in every hour
-func EnsureWiki(wikiURL string) (err error) {
-	if WikiExists() {
-		ctx, cancelFunc := context.WithCancel(context.Background())
-		defer cancelFunc()
-		err = pullHourly(ctx)
+const wikiDir = "./wiki"
+
+func (m *MongoStorer) PullWiki(wikiURL string) (err error) {
+	if m.WikiExists() {
+		var (
+			repo  *git.Repository
+			wtree *git.Worktree
+		)
+
+		repo, err = git.PlainOpen(wikiDir)
+		if err != nil {
+			return
+		}
+
+		wtree, err = repo.Worktree()
+		if err != nil {
+			return
+		}
+
+		wtree.Pull(&git.PullOptions{RemoteName: "origin"})
 	} else {
-		_, err = git.PlainClone("./wiki", false, &git.CloneOptions{
+		_, err = git.PlainClone(wikiDir, false, &git.CloneOptions{
 			URL: wikiURL,
 		})
 	}
-	return err
-}
-
-func pullHourly(ctx context.Context) (err error) {
-	var (
-		repo  *git.Repository
-		wtree *git.Worktree
-	)
-
-	repo, err = git.PlainOpen("./wiki")
-	if err != nil {
-		return
-	}
-	wtree, err = repo.Worktree()
-	if err != nil {
-		return
-	}
-	go func() {
-		tick := time.NewTicker(time.Hour)
-
-		defer tick.Stop()
-
-		for {
-			select {
-			case <-tick.C:
-				wtree.Pull(&git.PullOptions{RemoteName: "origin"})
-			case <-ctx.Done():
-				return
-			}
-		}
-	}()
 	return
 }
 
 // WikiExists checks if wiki directory exists on root path & is a git repository
-func WikiExists() bool {
-	_, err := git.PlainOpen("./wiki")
+func (m *MongoStorer) WikiExists() (exists bool) {
+	_, err := git.PlainOpen(wikiDir)
 	if err == git.ErrRepositoryNotExists {
-		return false
+		exists = false
+	} else {
+		exists = true
 	}
-	return true
+	return
 }
