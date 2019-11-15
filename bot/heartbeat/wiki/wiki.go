d6f3ddf9a43d8b157932287dcb2c583365fde2c7
Lasha Kanteladze <lashakanteladze339@protonmail.com>
2019-11-15 17:14:27 +0400

    Wiki rework (#148)
    * make .gitignore only ignore wiki directory in only root directory
    
    * rewrote wiki pulling workflow
    
    * add wiki related storage functions in the Stoner and Memory interfaces
    
    * use local variables instead of importing external storage package
    
    * add wiki heartbeat

---
+++ bot/heartbeat/wiki/wiki.go
@@ -0,0 +1,47 @@
+package wiki
+
+import (
+	"github.com/Southclaws/cj/bot/heartbeat/common"
+	"github.com/Southclaws/cj/discord"
+	"github.com/Southclaws/cj/forum"
+	"github.com/Southclaws/cj/storage"
+	"github.com/Southclaws/cj/types"
+)
+
+// Wiki holds wiki git repository
+type Wiki struct {
+	Config  *types.Config
+	Discord *discord.Session
+	Storage storage.Storer
+	Forum   *forum.ForumClient
+}
+
+//nolint:golint
+func (w *Wiki) Init(
+	config *types.Config,
+	discord *discord.Session,
+	api storage.Storer,
+	fc *forum.ForumClient,
+) (err error) {
+	w.Config = config
+	w.Storage = api
+	w.Discord = discord
+	w.Forum = fc
+	return
+}
+
+//nolint:golint
+func (w *Wiki) Register() (actions []common.Action) {
+	return []common.Action{
+		{
+			Schedule: "@every 1h",
+			Chance:   1,
+			Call:     w.pullWiki,
+		},
+	}
+}
+
+func (w *Wiki) pullWiki() (err error) {
+	err = w.Storage.PullWiki(w.Config.WikiURL)
+	return
+}
