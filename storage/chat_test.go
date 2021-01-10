51c9c23c4c61fe3d0a50d579f6d4aff372a33faa
NotUnlikeTheWaves <hleistra@gmail.com>
2021-01-10 13:02:14 +0100

    Fix tests

--- storage/chat_test.go
+++ storage/chat_test.go
@@ -12,25 +12,26 @@ func TestAPI_RecordChatLog(t *testing.T) {
 		discordUserID  string
 		discordChannel string
 		message        string
+		messageID      string
 	}
 	tests := []struct {
 		args    args
 		wantErr bool
 	}{
-		{args{"u1", "c1", "m0"}, false},
-		{args{"u2", "c1", "m1"}, false},
-		{args{"u2", "c1", "m2"}, false},
-		{args{"u1", "c2", "m3"}, false},
-		{args{"u1", "c2", "m4"}, false},
-		{args{"u3", "c2", "m5"}, false},
-		{args{"u2", "c1", "m6"}, false},
-		{args{"u3", "c2", "m7"}, false},
-		{args{"u2", "c1", "m8"}, false},
-		{args{"u4", "c2", "m9"}, false},
+		{args{"u1", "c1", "m0", "i0"}, false},
+		{args{"u2", "c1", "m1", "i1"}, false},
+		{args{"u2", "c1", "m2", "i2"}, false},
+		{args{"u1", "c2", "m3", "i3"}, false},
+		{args{"u1", "c2", "m4", "i4"}, false},
+		{args{"u3", "c2", "m5", "i5"}, false},
+		{args{"u2", "c1", "m6", "i6"}, false},
+		{args{"u3", "c2", "m7", "i7"}, false},
+		{args{"u2", "c1", "m8", "i8"}, false},
+		{args{"u4", "c2", "m9", "i9"}, false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.args.message, func(t *testing.T) {
-			err := api.RecordChatLog(tt.args.discordUserID, tt.args.discordChannel, tt.args.message)
+			err := api.RecordChatLog(tt.args.discordUserID, tt.args.discordChannel, tt.args.message, tt.args.messageID)
 			assert.NoError(t, err)
 		})
 	}
