992cfe25ab612762c3058f66ad239e0d7309bcbf
NotUnlikeTheWaves <hleistra@gmail.com>
2021-05-17 00:35:12 +0200

    Move single use code to where it's used

--- bot/commands/cmd_toprep.go
+++ bot/commands/cmd_toprep.go
@@ -1,13 +1,17 @@
 package commands
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/bwmarrin/discordgo"
 	"github.com/pkg/errors"
+	"go.uber.org/zap"
 
-	"github.com/Southclaws/cj/bot/heartbeat/stats"
+	"github.com/Southclaws/cj/discord"
+	"github.com/Southclaws/cj/storage"
 	"github.com/Southclaws/cj/types"
 )
-
 
 func (cm *CommandManager) commandTopRep(
 	args string,
@@ -22,11 +26,38 @@ func (cm *CommandManager) commandTopRep(
 	if err != nil {
 		return false, errors.Wrap(err, "failed to get message rankings")
 	}
-	rankings, err := stats.FormatReactionRankings(top, cm.Discord)
+	rankings, err := FormatReactionRankings(top, cm.Discord)
 	if err != nil {
 		return
 	}
 
 	_, err = cm.Discord.S.ChannelMessageSendEmbed(message.ChannelID, rankings)
 	return
+}
+
+func FormatReactionRankings(r []storage.TopReactionEntry, s *discord.Session) (embed *discordgo.MessageEmbed, err error) {
+	statsMessage := strings.Builder{}
+	statsMessage.WriteString("Statistics\n\n") //nolint:errcheck
+
+	embed = &discordgo.MessageEmbed{Color: 0x3498DB}
+	var user *discordgo.User
+	if len(r) == 0 {
+		statsMessage.WriteString("There are no entries to display here!")
+	}
+	for i, tm := range r {
+		var username string
+		user, err = s.S.User(tm.UserID)
+		if err != nil {
+			zap.L().Warn("failed to get user", zap.Error(err), zap.String("user_id", tm.UserID))
+			username = tm.UserID
+		} else {
+			username = user.Username
+		}
+
+		statsMessage.WriteString(fmt.Sprintf("%d. **<%s>** - %s x %d\n", i+1, username, tm.Reaction, tm.Counter)) //nolint:errcheck
+	}
+
+	embed.Description = statsMessage.String()
+
+	return embed, nil
 }
